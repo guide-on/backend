d64d8964b8e99fc6e04e4ca128fed08bec840660
dudujh0106 <rkdwlgh01@naver.com>
2025-09-07 03:29:21 +0900

    feat: 지역관련 기능 구현

---
+++ src/main/java/com/guideon/region/mapper/RegionMapper.java
@@ -0,0 +1,9 @@
+package com.guideon.region.mapper;
+
+import com.guideon.region.dto.SidoDTO;
+
+import java.util.List;
+
+public interface RegionMapper {
+    List<SidoDTO> selectSidoList();
+}
---
+++ src/main/java/com/guideon/region/service/RegionService.java
@@ -0,0 +1,9 @@
+package com.guideon.region.service;
+
+import com.guideon.region.dto.SidoDTO;
+
+import java.util.List;
+
+public interface RegionService {
+    List<SidoDTO> getSidoList();
+}
